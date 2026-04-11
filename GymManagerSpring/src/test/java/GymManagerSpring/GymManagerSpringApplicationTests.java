afc74c5dbd703075dbacb3cb595cd9e91cde82d9
jessika135 <jessicamanrique1995@gmail.com>
2026-04-11 01:39:21 -0500

    Add files via upload

---
+++ GymManagerSpring/src/test/java/GymManagerSpring/GymManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package GymManagerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
